5df8b4c9cec4310c2b3cec3673ab21a75fea425e
Bemourn <70115233+koenib@users.noreply.github.com>
2021-11-08 19:17:02 -0700

    create option buttons and logic to check correct answer

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,6 +1,6 @@
 const questions = [{
     question: 'Light recon vehicles with rear mounted chainguns are called:',
-    options: ['Banshees', 'Tanks', 'Mongooses', 'Warthogs'],
+    options: ['Banshees', 'Tanks', 'Mongoose', 'Warthogs'],
     answer: 3,
 }, {
     question: 'The strongest Covenany troops are called:',
@@ -20,30 +20,57 @@ const questions = [{
     answer: 2,
 }, ]
 
-let timeLeft = 5;
+let timeLeft = 90;
 let timerSpan = document.querySelector("#timer");
 timerSpan.innerHTML = timeLeft;
 let currentQuestion = 0;
 let qTitle = document.querySelector("#question-title");
-let qContainer = document.querySelector("#question-container");
-console.log(qTitle);
+let qContainer = document.querySelector("#container");
+let rSpan = document.querySelector("#result");
+let e;
 
 // Insert the question into the header
 qTitle.innerHTML = questions[currentQuestion].question;
 
 // Add possible answers to the buttons
 for (let i = 0; i < questions[currentQuestion].options.length; i++) {
-    qContainer.children[i+1].innerHTML = questions[currentQuestion].options[i];
-}
 
-function countDown() {
-    timeLeft--;
-    timerSpan.innerHTML = timeLeft;
-    console.log(timeLeft);
-
-    if (timeLeft === 0 || timeLeft < 0) {
-        window.location.href = 'hs.html';
+    // Display each possible answer to the displayed question
+    qContainer.children[1].children[i].innerHTML = `${i+1}. ${questions[currentQuestion].options[i]}`;
+    
+    // If the current option is the correct answer, set data value to true
+    if (i + 1 === questions[currentQuestion].answer) {
+        qContainer.children[1].children[i+1].dataset.answer = true;
     }
 }
 
-setInterval(countDown, 1000);
+// Listen for click on an answer
+const answers = document.getElementsByTagName("li");
+
+for (let ans of answers) {
+    ans.addEventListener("click", playQuiz);
+}
+
+function playQuiz(event){
+
+    // Check if the option clicked is the correct answer,
+    // if so - display "Correct!" and move to the next question.
+    if (event.target.dataset.answer === "true") {
+        rSpan.innerHTML = "Correct!";
+    }
+    else {
+        rSpan.innerHTML = "Wrong!";
+    }
+}
+
+// function countDown() {
+//     timeLeft--;
+//     timerSpan.innerHTML = timeLeft;
+//     console.log(timeLeft);
+
+//     if (timeLeft === 0 || timeLeft < 0) {
+//         window.location.href = 'hs.html';
+//     }
+// }
+
+// setInterval(countDown, 1000);
