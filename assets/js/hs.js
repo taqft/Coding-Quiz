92772fb30b1dce5a74df498df69f66c35db827ec
Bemourn <70115233+koenib@users.noreply.github.com>
2021-11-13 18:00:06 -0700

    stable version with new comments

--- assets/js/hs.js
+++ assets/js/hs.js
@@ -1,1 +1,36 @@
-// Highscores logic
+// select highscores list items and buttons
+const highscoresEl = $('#highscores-list');
+const clearBtn = $('#clear-btn');
+const backBtn = $('#back-btn');
+
+// remove all <li> tags from the screen when the
+// "clear highscores" button is pressed, and
+// remove all records from storage
+clearBtn.on('click', function () {
+    $('li').each(function () {
+        $(this).remove();
+    });
+    // clear localStorage
+    localStorage.setItem("highScores", "[]");
+});
+
+// go back to the quiz home page
+backBtn.on('click', function () {
+    window.location.href = 'index.html';
+});
+
+// grab existing scores from storage
+const highScores = JSON.parse(localStorage.getItem("highScores"));
+
+// sort the array of highScores in descending order
+highScores.sort(function (a, b) {
+    return b.score - a.score;
+});
+
+// dynamically add all highscores from storage + the new highscore to the screen
+for (var i = 0; i < highScores.length; i++) {
+    var highScoresItem = $('<li>')
+        .text(`${i+1}. ${highScores[i].initials} - ${highScores[i].score}`)
+        .addClass('highscore-item');
+    highscoresEl.append(highScoresItem);
+}
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,9 +1,10 @@
+// import list of questions
 const questions = [{
     question: 'Light recon vehicles with rear mounted chainguns are called:',
     options: ['Banshees', 'Tanks', 'Mongoose', 'Warthogs'],
     answer: 3,
 }, {
-    question: 'The strongest Covenany troops are called:',
+    question: 'The strongest Covenant troops are called:',
     options: ['Brutes', 'Elites', 'Covenants', 'Grunts'],
     answer: 1,
 }, {
@@ -20,56 +21,140 @@ const questions = [{
     answer: 2,
 }, ]
 
+// initialize global variables
 let timeLeft = 90;
-let timerSpan = document.querySelector("#timer");
-timerSpan.innerHTML = timeLeft;
 let currentQuestion = 0;
-let qTitle = document.querySelector("#question-title");
-let qContainer = document.querySelector("#container");
-let rSpan = document.querySelector("#result");
+let userScore = {};
 
-// Insert the question into the header
-qTitle.innerHTML = questions[currentQuestion].question;
+// select elements to be changed dynamically 
+const timerSpan = document.querySelector("#timer");
+timerSpan.innerHTML = timeLeft;
+const qTitle = document.querySelector("#question-title");
+const qContainer = document.querySelector("#container");
+const rSpan = document.querySelector("#result");
+const containerEl = $("#container");
 
-// Add possible answers to the buttons
-for (let i = 0; i < questions[currentQuestion].options.length; i++) {
+// dynamically add the current question from the list of questions onto the page
+function displayQuestion(currentQuestion) {
 
-    // Display each possible answer to the displayed question
-    qContainer.children[1].children[i].innerHTML = `${i+1}. ${questions[currentQuestion].options[i]}`;
-    
-    // If the current option is the correct answer, set data value to true
-    if (i + 1 === questions[currentQuestion].answer) {
-        qContainer.children[1].children[i+1].dataset.answer = true;
+    // Insert the question into the header
+    qTitle.innerHTML = questions[currentQuestion].question;
+
+    // Add possible answers to the list items (option buttons)
+    for (let i = 0; i < questions[currentQuestion].options.length; i++) {
+
+        // Display each possible answer to the displayed question
+        qContainer.children[1].children[i].innerHTML = `${i+1}. ${questions[currentQuestion].options[i]}`;
+        qContainer.children[1].children[i].dataset.answer = false;
+        // If the current option is the correct answer, set data value to true
+        if (i === questions[currentQuestion].answer) {
+            qContainer.children[1].children[i].dataset.answer = true;
+        }
     }
 }
 
 // Listen for click on an answer
-const answers = document.getElementsByTagName("li");
+const answersEl = $("#question-options");
 
-for (let ans of answers) {
-    ans.addEventListener("click", playQuiz);
-}
+// Refactored: Add event listener to parent <ul> instead of individual <li>'s.
+// Avoid iterating over all list items with event listeners, since this is very expensive
+answersEl.on('click', '.option', function (event) {
+    playQuiz(event);
+});
 
-function playQuiz(event){
+function playQuiz(event) {
 
     // Check if the option clicked is the correct answer,
     // if so - display "Correct!" and move to the next question.
     if (event.target.dataset.answer === "true") {
         rSpan.innerHTML = "Correct!";
-    }
-    else {
+    } else {
         rSpan.innerHTML = "Wrong!";
+        timeLeft = timeLeft - 10;
     }
+    currentQuestion++;
+    // check if we've reached the end of the list of questions,
+    // if not, display the next question in the list
+    if (currentQuestion < questions.length) {
+        displayQuestion(currentQuestion);
+    } else { // Immediately call countDown since the quiz is over
+        // excluding this will allow extra clicks, subtracting more score than expected.
+        countDown();
+    }
+
 }
 
-// function countDown() {
-//     timeLeft--;
-//     timerSpan.innerHTML = timeLeft;
-//     console.log(timeLeft);
+function countDown() {
+    timeLeft--;
+    timerSpan.innerHTML = timeLeft;
 
-//     if (timeLeft === 0 || timeLeft < 0) {
-//         window.location.href = 'hs.html';
-//     }
-// }
+    // check if the time has run out, either from a timeout
+    // or from losing too many points on incorrect answers
+    if (timeLeft === 0 || timeLeft < 0 || questions.length === currentQuestion) {
 
-// setInterval(countDown, 1000);
+        // select the currently displayed quiz question and option list
+        // then remove it from the screen to dynamically add highscores
+        let qh2 = document.getElementById("question-title");
+        let oul = document.getElementById("question-options");
+        qh2.remove();
+        oul.remove();
+        // remove the 1 second interval for countDown()
+        clearInterval(intervalId);
+        // display the quiz 
+        const newTitle = document.createElement('h2');
+        newTitle.textContent = "Quiz Over";
+        const newText = document.createElement('p');
+        newText.textContent = `Your final score is ${timeLeft}.`;
+
+        // create the buttons for highscore submission
+        const inputLabel = $('<label>')
+            .attr('for', 'Initials')
+            .text("Initials: ");
+
+        const inputBox = $('<input>')
+            .attr('type', 'text')
+            .attr('name', 'Initials');
+
+        const submitButton = $('<button>')
+            .attr('type', 'button')
+            .addClass('submit-button')
+            .text("Submit");
+
+        // add the elements to the screen (prepended)
+        containerEl.prepend(submitButton);
+        containerEl.prepend(inputBox);
+        containerEl.prepend(inputLabel);
+        containerEl.prepend(newText);
+        containerEl.prepend(newTitle);
+
+        // store the submitted initials and score, call submitScore()
+        submitButton.on('click', function () {
+            let userInitials = $('input[name=Initials]').val();
+            userScore = {
+                'initials': userInitials,
+                'score': timeLeft
+            };
+            submitScore();
+        });
+
+    }
+}
+// set an interval of one second
+let intervalId = setInterval(countDown, 1000);
+
+function submitScore() {
+    // grab any existing localStorage data for scores, or an empty array if it is null
+    let highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
+
+    // add the new score
+    highScores.push(userScore);
+
+    // set the local storage to include the new score
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+
+    // redirect to the highscores page
+    window.location.href = 'hs.html';
+}
+
+// display the current question when the page loads
+displayQuestion(currentQuestion);
